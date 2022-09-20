757696f69a6df298c5ecc290872375a1fa572251
ayronman99 <itechman32@gmail.com>
2022-09-20 17:20:06 +0800

    Upload edited version of Blazor Pokedex Andre Lopes
    -added navigation for next and previous Pokemon

---
+++ Configurations/PokeApiOptions.cs
@@ -0,0 +1,9 @@
+
+namespace Pokedex.Configurations;
+
+public class PokeApiOptions
+{
+    public const string Name = "PokeApi";
+
+    public Uri BaseAddress { get; set; }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+
+using MatBlazor;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Pokedex;
+using Pokedex.Configurations;
+using Pokedex.Clients;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddMatBlazor();
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+var pokeApiOptions = builder.Configuration
+                         .GetSection(PokeApiOptions.Name)
+                         .Get<PokeApiOptions>();
+builder.Services.AddHttpClient<PokeClient>(options =>
+{
+    options.BaseAddress = pokeApiOptions.BaseAddress;
+});
+
+await builder.Build().RunAsync();
